9d01ceff5174eb02530cedb139d41d369f55c6bd
Carlo Field <carlo@swiss.dev>
2023-02-14 11:25:22 +0100

    fix build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   // tagline: 'Dinosaurs are cool',
   url: 'https://docs.loadout.report',
-  baseUrl: '',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
